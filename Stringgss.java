718492a33ae8fa669454cd0783b3a2bca070d425
arpita-debnath <51192957+arpita-debnath@users.noreply.github.com>
2019-06-06 22:39:55 +0530

    Add files via upload

---
+++ Stringgss.java
@@ -0,0 +1,20 @@
+package in.ac.sharda;
+
+public class Stringgss {
+
+	public static void main(String[] args) {
+		String str  = "this is a string one";
+		String str2 ="two";
+		if (str==null || str.length()==0) {
+			System.out.println("blank");
+		} else if(str.equals(str2)) {
+		     System.out.println("equal");
+	} else {
+			  System.out.println("not blank");
+		    	
+		    	 
+		        }
+
+	}
+
+}
